1d44e3f81066a06321781e8f39a75bf972ca12d4
Jack Bjerregaard <jack@Jacks-MBP.local>
2025-11-27 12:25:38 +0100

    abc

--- src/disassemble.c
+++ src/disassemble.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void disassemble(uint32_t addr, uint32_t instruction, char* result, size_t buf_size, struct symbols* symbols) {
-
+void disassemble(uint32_t addr, uint32_t instruction, char *result, size_t buf_size,
+                 struct symbols *symbols) {
+  uint32_t opcode =  instruction & 0x7F; //takes the opcode from [0:6]
 }
