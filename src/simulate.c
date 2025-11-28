5a7359dd190d331ec9a7d0874518c5e2239da290
Jack Bjerregaard <jack@Jacks-MBP.local>
2025-11-28 18:19:20 +0100

    added break statements for fall trhough

--- src/simulate.c
+++ src/simulate.c
@@ -32,18 +32,24 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct 
     case 0x37:  // 55 decimal - LUI (Load Upper Immediate)
     case 0x17:  // 23 decimal - AUIPC (Add Upper Immediate to PC)
       handle_type_U(mem, start_addr, log_file, symbols);
+      break; 
     case 0x6F:  // 111 decimal - JAL (Jump and Link)
       handle_type_J(mem, start_addr, log_file, symbols);
+      break; 
     case 0x63:  // 99 decimal - Branches (BEQ, BNE, BLT, BGE, BLTU, BGEU)
       handle_type_B(mem, start_addr, log_file, symbols);
+      break; 
     case 0x67:  // 103 decimal - JALR (Jump and Link Register)
     case 0x03:  // 3 decimal - Loads (LB, LH, LW, LBU, LHU)
     case 0x13:  // 19 decimal - I-type arithmetic (ADDI, SLTI, XORI, ORI, ANDI, SLLI, SRLI, SRAI)
     case 0x73:  // 115 decimal - System (ECALL, EBREAK)
       handle_type_I(mem, start_addr, log_file, symbols);
+      break; 
     case 0x23:  // 35 decimal - Stores (SB, SH, SW)
       handle_type_S(mem, start_addr, log_file, symbols);
+      break; 
     case 0x33:  // 51 decimal - R-type (ADD, SUB, etc.) and RV32M (MUL, DIV, etc.)
       handle_type_R(mem, start_addr, log_file, symbols);
+      break; 
   }
 }
