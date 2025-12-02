61dd79b2e5bbfbd8a1322245b3a06fae5d02601e
Jack Bjerregaard <jack@Jacks-MBP.local>
2025-12-02 10:16:07 +0100

    ah 2

--- src/simulate.c
+++ src/simulate.c
@@ -29,27 +29,27 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct 
 
   // 3
   switch (opcode) {
-    case 0x37:  // 4
-    case 0x17:  // 5
+    case 0x37:  // 0110111 - U-type
+    case 0x17:  // 0010111 - U-type
       handle_type_U(mem, start_addr, log_file, symbols);
       break;
-    case 0x6F:  // 6
+    case 0x6F:  // 1101111 - J-type
       handle_type_J(mem, start_addr, log_file, symbols);
       break;
-    case 0x63:  // 7
+    case 0x63:  // 1100011 - B-type
       handle_type_B(mem, start_addr, log_file, symbols);
       break;
-    case 0x67:  // 8
-    case 0x03:  // 9
-    case 0x13:  // 10
-    case 0x73:  // 11
+    case 0x67:  // 1100111 - I-type
+    case 0x03:  // 0000011 - I-type
+    case 0x13:  // 0010011 - I-type
+    case 0x73:  // 1110011 - I-type
       handle_type_I(mem, start_addr, log_file, symbols);
       break;
-    case 0x23:  // 12
+    case 0x23:  // 0100011 - S-type
       handle_type_S(mem, start_addr, log_file, symbols);
       break;
-    case 0x33:  // 13
+    case 0x33:  // 0110011 - R-type
       handle_type_R(mem, start_addr, log_file, symbols);
-      break; 
+      break;
   }
 }
