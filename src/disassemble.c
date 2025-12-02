101a25bd9cd98cc392e2ef2e40c2e9452a9fc9c9
Roni Temizsoy <Itzrondy@gmail.com>
2025-12-02 14:55:16 +0100

    Setup function structure for type switch case

--- src/disassemble.c
+++ src/disassemble.c
@@ -6,6 +6,30 @@
 #include "disassemble.h"
 #include "read_elf.h"
 
+void handle_type_U(uint32_t instruction, char *result) {}
+
+void handle_type_J(uint32_t instruction, char *result) {}
+
+void handle_type_B(uint32_t instruction, char *result) {}
+
+void handle_type_I(uint32_t instruction, char *result) {
+  uint32_t rd = (instruction >> 7) & 0x1F;
+  uint32_t imm = ((instruction >> 20) & 0xFFF);
+
+  // check the last bit of imm and check if we need to set negative
+  if (imm & 0x800) {
+    // Set all other bits after the imm bits to 1 to indicate negative (two's complement)
+    imm |= 0xFFFFF000;
+  }
+
+  printf("%d\t", rd);
+  printf("%d", imm);
+}
+
+void handle_type_S(uint32_t instruction, char *result) {}
+
+void handle_type_R(uint32_t instruction, char *result) {}
+
 void disassemble(uint32_t addr, uint32_t instruction, char *result, size_t buf_size,
                  struct symbols *symbols) {
 
@@ -22,31 +46,25 @@ void disassemble(uint32_t addr, uint32_t instruction, char *result, size_t buf_s
   switch (opcode) {
     case 0x37:  // 0110111 - U-type
     case 0x17:  // 0010111 - U-type
+      handle_type_U(instruction, result);
       break;
     case 0x6F:  // 1101111 - J-type
+      handle_type_J(instruction, result);
       break;
     case 0x63:  // 1100011 - B-type
+      handle_type_B(instruction, result);
       break;
     case 0x67:  // 1100111 - I-type
     case 0x03:  // 0000011 - I-type
     case 0x13:  // 0010011 - I-type
     case 0x73:  // 1110011 - I-type
-      uint32_t rd = (instruction >> 7) & 0x1F;
-      uint32_t imm = ((instruction >> 20) & 0xFFF);
-
-      // check the last bit of imm and check if we need to set negative
-      if (imm & 0x800) {
-        // Set all other bits after the imm bits to 1 to indicate negative (two's complement)
-        imm |= 0xFFFFF000;
-      }
-
-      //printf("%ud", rd);
-      printf("%d", imm);
+      handle_type_I(instruction, result);
       break;
     case 0x23:  // 0100011 - S-type
+      handle_type_S(instruction, result);
       break;
     case 0x33:  // 0110011 - R-type
+      handle_type_R(instruction, result);
       break;
   }
-
 }
